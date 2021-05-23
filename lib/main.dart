d94392309dc169839e42255f890d8da6e89a3e40
peilin <peilin@gmail.com>
2021-05-23 18:46:50 +0800

    changed to green color

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      theme: ThemeData(primarySwatch: Colors.purple),
+      theme: ThemeData(primarySwatch: Colors.green),
       home: RandomWords(),
     );
   }
