cfc9e56609568f2acec85183d1255fc3f7cdb2a3
peilin <peilin@gmail.com>
2021-05-23 18:53:30 +0800

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: RandomWords(),
     );
   }
