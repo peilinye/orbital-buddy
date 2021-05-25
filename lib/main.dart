3ff053798f754d93d4deeb7fece8cb1eeeb75357
peilin <peilinye18@gmail.com>
2021-05-25 23:38:52 +0800

    centralised start

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _biggerFont = TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
-        label: const Text('start'),
-        icon: const Icon(Icons.alarm_on),
-        backgroundColor: Colors.lightGreen.shade300,
+      body: Column(children: const <Widget>[
+        // menu and $$ here
+        // animals and items here
+      ]),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 100),
+        child: FloatingActionButton.extended(
+          label: const Text('start'),
+          icon: const Icon(Icons.alarm_on),
+          backgroundColor: Colors.lightGreen.shade400,
+          onPressed: () {},
+        ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
